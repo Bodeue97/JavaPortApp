79f588ffb9bf015df42494c2dbbf1bd7e77f8d61
Bodeue97 <lukaszebo@gmail.com>
2025-06-27 22:10:51 +0200

    initial commit

---
+++ src/main/java/pl/bohdanlukaszewicz/JavaPortApp/JavaPortAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bohdanlukaszewicz.JavaPortApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPortAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPortAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bohdanlukaszewicz/JavaPortApp/JavaPortAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bohdanlukaszewicz.JavaPortApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPortAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
